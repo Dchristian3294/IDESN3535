ce85ed0858c656383062d99d50bd03ea7d9a98a8
Dan Christian <dchrist6@lesley.edu>
2016-10-25 16:27:52 -0400

    asdf

---
+++ practice/practice.js
@@ -0,0 +1,10 @@
+console.log("The user has submitted their contact information");
+
+function validate_form_data() {
+
+    var first_name = document.getElementById('first_name');
+    console.log("first_name");
+
+    var email = document.getElementById('email');
+    console.log("email");
+}
